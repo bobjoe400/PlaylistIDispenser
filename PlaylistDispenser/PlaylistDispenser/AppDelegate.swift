2179d84733750fc73e3b79747c8f12e124c36978
TheFry <thisssparta6@gmail.com>
2015-11-18 14:01:02 -0800

    Configured Parse

--- PlaylistDispenser/PlaylistDispenser/AppDelegate.swift
+++ PlaylistDispenser/PlaylistDispenser/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("M1xybr69RYPdOYhMF02f4YMOTYfDPoVnsg065cjh",
+            clientKey: "QHiF4XuEtBzLH3NhbWkOpBQ6b9svCwdu6wtoFagp")
         return true
     }
 

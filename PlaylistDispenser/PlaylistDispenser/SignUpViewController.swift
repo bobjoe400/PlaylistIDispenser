be49fc350b0cc3e82ca342bc657abe4a300cdb0a
the-young-man <burgundycheese@gmail.com>
2015-11-19 16:38:39 -0800

    added sighnup
    fackengai

--- PlaylistDispenser/PlaylistDispenser/SignUpViewController.swift
+++ PlaylistDispenser/PlaylistDispenser/SignUpViewController.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import Parse
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func signUpButtonClicked(sender: AnyObject) {
+        if self.passwordTextField.text != self.confirmPasswordTextField.text {
+            let alert = UIAlertView()
+            alert.title = "Error"
+            alert.message = "Passwords entered do not match"
+            alert.addButtonWithTitle("Try again")
+            alert.show()
+        } else if (self.passwordTextField.text=="") || (self.confirmPasswordTextField.text=="") || (self.usernameTextField.text=="") || (self.emailTextField.text=="") {
+            let alert = UIAlertView()
+            alert.title = "Error"
+            alert.message = "Error: Blank Field"
+            alert.addButtonWithTitle("Try again")
+            alert.show()
+        } else {
+            let email = emailTextField.text
+            let username = usernameTextField.text
+            let password = passwordTextField.text
+            let signUpData = PFObject(className: "users")
+            signUpData["username"] = username
+            signUpData["password"] = password
+            signUpData["email"] = email
+            signUpData.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+                print(signUpData)
+            }
+            performSegueWithIdentifier("segueSignUptoLogin", sender: nil)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

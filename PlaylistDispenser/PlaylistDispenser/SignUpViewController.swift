6923177917a1699a1ebc9135374d1b9b3ba42409
zuoyuanh <student2@csc232x16.ad.calpoly.edu>
2015-12-04 03:47:24 -0800

    Image Picker

--- PlaylistDispenser/PlaylistDispenser/SignUpViewController.swift
+++ PlaylistDispenser/PlaylistDispenser/SignUpViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 import Parse
 
-class SignUpViewController: UIViewController, UITextFieldDelegate {
-
+class SignUpViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var profileImage: UIImage?
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var profileImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +35,24 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     @IBAction func cancelClicked(sender: AnyObject) {
         self.performSegueWithIdentifier("unwindToLogin", sender: nil)
     }
+    
+    @IBAction func selectPictureButtonClicked(sender: AnyObject) {
+        var pictureSelector = UIImagePickerController()
+        pictureSelector.delegate = self
+        pictureSelector.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        self.presentViewController(pictureSelector, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        print("pickerfunctioncalled")
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            profileImageView.contentMode = .ScaleAspectFit
+            profileImageView.image = pickedImage
+            profileImage = pickedImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     func signupCheck(){
         if self.passwordTextField.text != self.confirmPasswordTextField.text {
             let alert = UIAlertView()
@@ -53,6 +74,11 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
             let username = usernameTextField.text
             let password = passwordTextField.text
             let signUpData = PFObject(className: "users")
+            if profileImage != nil {
+                let imageData = UIImagePNGRepresentation(resizeImage(profileImage!)!)
+                let imageFile = PFFile(name: username!+".png", data:imageData!)
+                signUpData["profilePicture"] = imageFile
+            }
             signUpData["username"] = username
             signUpData["password"] = password
             signUpData["email"] = email
@@ -62,6 +88,42 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
             }
             self.performSegueWithIdentifier("unwindToLogin", sender: nil)
         }
+    }
+    
+    func resizeImage(image: UIImage?) -> UIImage?
+    {
+        var actualHeight: Float = Float(image!.size.height)
+        var actualWidth: Float = Float(image!.size.width)
+        let maxHeight: Float = 300.0
+        let maxWidth: Float = 400.0
+        var imgRatio: Float = actualWidth/actualHeight
+        let maxRatio: Float = maxWidth/maxHeight
+        let compressionQuality: CGFloat = 0.5
+        if actualHeight > maxHeight || actualWidth > maxWidth {
+            if imgRatio < maxRatio {
+                imgRatio = maxHeight / actualHeight
+                actualWidth = imgRatio * actualWidth
+                actualHeight = maxHeight
+            } else if imgRatio > maxRatio {
+                imgRatio = maxWidth / actualWidth;
+                actualHeight = imgRatio * actualHeight;
+                actualWidth = maxWidth;
+            } else {
+                actualHeight = maxHeight;
+                actualWidth = maxWidth;
+            }
+        }
+        let rect = CGRectMake(0.0, 0.0, image!.size.width, image!.size.height)
+        UIGraphicsBeginImageContext(rect.size)
+        image?.drawInRect(rect)
+        let img: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        let imageData = UIImageJPEGRepresentation(img, compressionQuality)
+        UIGraphicsEndImageContext()
+        let resultImage = UIImage(data: imageData!)
+        if let resultImage = resultImage {
+            return resultImage
+        }
+        return nil
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
